6ba0b32da1ca12083cd7b69129ec94327f8938ed
9cc9 <cyx199021@163.com>
2025-04-02 14:16:31 +0800

    Initial Commit

---
+++ AICalender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AICalender
+//
+//  Created by 贝贝 on 2025/4/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AICalenderTests/AICalenderTests.swift
@@ -0,0 +1,17 @@
+//
+//  AICalenderTests.swift
+//  AICalenderTests
+//
+//  Created by 贝贝 on 2025/4/2.
+//
+
+import Testing
+@testable import AICalender
+
+struct AICalenderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
